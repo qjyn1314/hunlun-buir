ff30aea6dcc44bcb403cc441b6033b3a6f588453
wangjunming <wangjunming@iciyun.com>
2020-02-06 10:11:00 +0800

    添加springboot-Admin监控

---
+++ hulunbuir-admin/src/test/java/com/hulunbuir/clam/admin/HulunbuirAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulunbuir.clam.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulunbuirAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
