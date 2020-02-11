5f8f3531d3219a8a8543674103df34920f8ecaf1
wangjunming <wangjunming@iciyun.com>
2020-02-11 15:53:34 +0800

    添加redis配置

--- hulunbuir-afternoon/src/test/java/com/hulunbuir/clam/afternoon/HulunBuirAfternoonApplicationTests.java
+++ hulunbuir-afternoon/src/test/java/com/hulunbuir/clam/afternoon/HulunBuirAfternoonApplicationTests.java
@@ -1,13 +1,21 @@
 package com.hulunbuir.clam.afternoon;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class HulunBuirAfternoonApplicationTests {
 
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
+
     @Test
     void contextLoads() {
+
+
+
     }
 
 }
---
+++ hulunbuir-route/src/main/java/com/hulunbuir/clam/route/config/ShiroConfig.java
@@ -0,0 +1,19 @@
+package com.hulunbuir.clam.route.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * Explain:
+ * </p >
+ *
+ * @author wangjunming
+ * @since 2020-02-06 14:46
+ */
+@Configuration
+public class ShiroConfig {
+
+
+
+
+}
