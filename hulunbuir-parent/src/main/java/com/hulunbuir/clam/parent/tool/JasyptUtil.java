9301540d0682749fb62f71855d9b3ac8b5a796cc
qjyn1314 <qjyn1314@163.com>
2020-11-30 18:02:17 +0800

    添加生产配置文件

--- hulunbuir-parent/src/main/java/com/hulunbuir/clam/parent/tool/JasyptUtil.java
+++ hulunbuir-parent/src/main/java/com/hulunbuir/clam/parent/tool/JasyptUtil.java
@@ -100,10 +100,10 @@ public class JasyptUtil {
 //        encryptPwd = "MdQRVDiKXf4sS0lXp61SH5cvVdmn8FJw++ODKj0ojhY=";
         System.out.println("配置文件中jasypt.encryptor.password所配置的加密字符串是：" + ENCRYPT_PWD);
         //真实密码
-        final String password = "admin";
+        final String password = "jdbc:mysql://47.105.33.140:3307/evening_prod?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true";
         // 加密,打印出来的就是ENC(密码)的密码
         String pwd = encryptPwd(ENCRYPT_PWD, password);
-        pwd = "cTGBYZGJ6J2ikE2ECTwSX/R8KfGo8a42";
+//        pwd = "cTGBYZGJ6J2ikE2ECTwSX/R8KfGo8a42";
         System.out.println("配置文件中需要ENC(密码)配置的密码是：" + pwd);
         final String decyptPwd = decyptPwd(ENCRYPT_PWD, pwd);
         System.out.println("解密之后的真实密码：" + decyptPwd);
