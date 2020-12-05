d2124e64972ab4487cd75faef38695749c1351b5
qjyn1314 <qjyn1314@163.com>
2020-12-05 23:37:11 +0800

    添加启动类的注释

--- hulunbuir-evening/src/main/java/com/hulunbuir/clam/evening/HulunBuirEveningApplication.java
+++ hulunbuir-evening/src/main/java/com/hulunbuir/clam/evening/HulunBuirEveningApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 启动类
+ *
+ * @author wangjunming
+ * @since 2020/12/5 23:29
+ */
 @Slf4j
 @EnableDubbo
 @SpringBootApplication
